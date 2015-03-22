8384dfa9b4b933630514d7443c0525e17a94ee28
David Orme <djo@coconut-palm-software.com>
2015-03-22 16:42:19 -0500

    Customize tabbar groups so SBT and Shell buffers are in the User group

--- emacs-init.el
+++ emacs-init.el
@@ -411,6 +411,18 @@
 (global-set-key [M-left] 'tabbar-backward-tab)
 (global-set-key [M-right] 'tabbar-forward-tab)
 
+(defun my-tabbar-buffer-groups () ;; customize to show all normal files in one group
+  "Return the name of the tab group names the current buffer belongs to.
+There are two groups: Emacs buffers (those whose name starts with '*', plus
+dired buffers), and the rest.  This works at least with Emacs v24.2 using
+tabbar.el v1.7."
+  (list (cond ((string-equal "*sbt*" (substring (buffer-name) 0 5)) "user")
+              ((string-equal "*shell*" (buffer-name)) "user")
+              ((string-equal "*" (substring (buffer-name) 0 1)) "emacs")
+              ((eq major-mode 'dired-mode) "emacs")
+              (t "user"))))
+(setq tabbar-buffer-groups-function 'my-tabbar-buffer-groups)
+
 ;; tabbar coloring code...
 (set-face-attribute
    'tabbar-default nil
