4d5fbea6b442e273718723bda52779362a11b37e
David Orme <djo@coconut-palm-software.com>
2016-07-05 23:50:09 -0500

    Added convenience functions

--- predef.scala
+++ predef.scala
@@ -5,6 +5,37 @@ import shellSession._
 import ammonite.shell.PPrints._
 import ammonite.ops._
 import ammonite.shell._
+
 val beetl=root/'Users/'dorme/'shopsmart/"beetl-deps"/'beetl
-val pwd=wd
+val dill="djo@dill.local"
+val sage="dorme@sage.local"
+
+def pwd=wd
+
+def ssh(host : String) = { %ssh("-X", host) }
+def emacs(file : String) = { %emacs(file) }
+def vim(file : String) = { %vim(file) }
+
+def status = { %git('status) }
+def checkout(branch : String) = { %git('checkout, branch) }
+def gitclone(url : String) = { %git('clone, "--recursive", url) }
+def submodule(url : String) = { %git('submodule, 'add, url) }
+def update = { %git('submodule, 'update, "--init") }
+def branchTo(newBranch : String) = { %git('checkout, "-b", newBranch) }
+def branches = { %git 'branch}
+def pull = { %git 'pull}
+def merge(branch: String) = { %git('merge, branch) }
+def commit = { %git('commit) }
+def commitall = { %git('commit, "-a") }
+def commitall(message: String) = { %git('commit, "-a", "-m", message)}
+
+def bash = { %bash("-l") }
+
+def c(dir : Symbol) = { cd! dir }
+def c(dir : String) = { cd! dir }
+def c(dir : RelPath) = { cd! dir }
+def c(dir : Path) = { cd! dir }
+
+def l = { ls!  }
+
 ammonite.shell.Configure(repl, wd)
