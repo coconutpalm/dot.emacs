477137da3865e2517e4af17105953a6f529e14a5
David Orme <djo@coconut-palm-software.com>
2016-07-11 14:01:22 -0500

    Implicit converstions paths->String

--- predef.scala
+++ predef.scala
@@ -6,7 +6,18 @@ import ammonite.shell.PPrints._
 import ammonite.ops._
 import ammonite.shell._
 
+implicit def symbol2str(s : Symbol) : String = s.name
+implicit def path2str(p : Path) : String = {
+  p.segments.foldLeft("") { (cur,next) => cur + java.io.File.separator + next }
+}
+implicit def relpath2str(p : RelPath) : String = {
+  val absPath = wd/p
+  absPath.segments.foldLeft("") { (cur,next) => cur + java.io.File.separator + next }
+}
+
 val beetl=root/'Users/'dorme/'shopsmart/"beetl-deps"/'beetl
+val dim=root/'Users/'dorme/'dev/'datainmotion/"datainmotion-server"
+
 val dill="djo@dill.local"
 val sage="dorme@sage.local"
 
@@ -32,11 +43,10 @@ def commitall(message: String) = { %git('commit, "-a", "-m", message)}
 
 def bash = { %bash("-l") }
 
-def c(dir : Symbol) = { cd! dir }
 def c(dir : String) = { cd! dir }
-def c(dir : RelPath) = { cd! dir }
-def c(dir : Path) = { cd! dir }
 
 def l = { ls!  }
+def make = { %make }
+def predefs = { %vim home/".emacs.d"/"predef.scala" }
 
 ammonite.shell.Configure(repl, wd)
