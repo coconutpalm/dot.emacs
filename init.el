496af912aaee693124fe5725b850fe3ce4ef759e
David Orme <david.orme@rallyhealth.com>
2020-07-22 01:18:01 -0500

    Back to embers

--- init.el
+++ init.el
@@ -137,7 +137,7 @@
 (use-package base16-theme
   :ensure t)
 
-(load-theme 'base16-chalk t)
+(load-theme 'base16-embers t)
 
 ;; TODO magit colors
 ;; (magit-file-header ((t (:foreground "violet"))))
