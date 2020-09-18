7e789cb136d697dcfdd627630c997a513ce96135
David Orme <david.orme@rallyhealth.com>
2020-09-18 17:28:41 -0500

    WIP / cleanup cruft

--- init.el
+++ init.el
@@ -132,7 +132,7 @@
       auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 
-(package-install 'gnu-elpa-keyring-update)
+;(package-install 'gnu-elpa-keyring-update)
 
 
 ;; Color theme
