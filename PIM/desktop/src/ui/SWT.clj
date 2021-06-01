99beb24edabdff1b1029ec30914c65eaa40736ba
David Orme <djo@coconut-palm-software.com>
2021-05-31 21:18:15 -0500

    Extract SWT version to constant

--- PIM/desktop/src/ui/SWT.clj
+++ PIM/desktop/src/ui/SWT.clj
@@ -11,12 +11,18 @@
        (.toLowerCase))))
 
 
+;; Sure would be nice to have a newer version available through Maven...
+(def swt-version "4.3")
+(defn swt-lib [platform]
+  [(symbol "org.eclipse.swt" (str "org.eclipse.swt." (str platform))) swt-version])
+
+
 (def platform-swt-lib
   "The Maven coordinates of the current platform's SWT library."
   (cond
-    (= os-code "lin") '[org.eclipse.swt/org.eclipse.swt.gtk.linux.x86_64 "4.3"]
-    (= os-code "mac") '[org.eclipse.swt/org.eclipse.swt.cocoa.macosx.x86_64 "4.3"]
-    (= os-code "win") '[org.eclipse.swt/org.eclipse.swt.win32.win32.x86_64  "4.3"]
+    (= os-code "lin") (swt-lib 'gtk.linux.x86_64)
+    (= os-code "mac") (swt-lib 'cocoa.macosx.x86_64)
+    (= os-code "win") (swt-lib 'win32.win32.x86_64)
     :else              (throw (ex-info (str "Unsupported OS: " (System/getProperty "os.name"))))))
 
 
@@ -27,7 +33,7 @@
 
 (defonce display
   (if-not (= os-code "mac")
-    (Display.)))
+    (Display/getDefault)))
 
 
 (comment
