1e915b256e3dc68bee9b5d505fc6400bcb0217c8
David Orme <djo@coconut-palm-software.com>
2021-06-03 21:44:09 -0500

    Note more TODOs

--- PIM/insideout/src/insideout/core.clj
+++ PIM/insideout/src/insideout/core.clj
@@ -11,9 +11,22 @@
    [util.jobs                      :refer :all]))
 
 
+;; TODO
+;;
+;; Grab Boot's task framework and -main argument parser.
+;; Then `nrepl` becomes a task; `dynamo` (maybe) becomes a task.
+;; Programmers can write their own tasks.
+
+;; Find sources via searching `src/main/clojure` and `src`.
+;; `app.clj` is the program's main.  Search for it in the
+;; source folders, require it, and call its `-main`.
+
+;; Make a small fast native executable that communicates with
+;; dynamo over a unix domain socket (?) for monitoring/operability?
+
 (defn start! []
   (println "Starting..."))
 
 (defn -main
   "public static void main..."
-  [] (start!))
+  [args] (start!))
