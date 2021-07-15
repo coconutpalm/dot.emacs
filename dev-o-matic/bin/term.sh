1216fe702dcb211a3c7c614c8b6bd245df0c36c9
David Orme <djo@coconut-palm-software.com>
2021-07-15 13:00:14 -0500

    Use xfce4-terminal

--- dev-o-matic/bin/term.sh
+++ dev-o-matic/bin/term.sh
@@ -3,4 +3,4 @@ set -e
 source ~/bin/devenv-utils
 
 xhost + 127.0.0.1 > /dev/null 2>&1
-ssh -q -Y -p 2222 localhost mlterm -L &
+ssh -q -Y -p 2222 localhost 'xfce4-terminal --disable-server -e "bash -l"' >>/tmp/dev-term-$RANDOM 2>&1 &
