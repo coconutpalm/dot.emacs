333b2c55c11a19f566dcd31d3a51ea2aead1b1a3
David Orme <djo@coconut-palm-software.com>
2021-08-19 22:35:07 -0500

    Add default help text

--- PIM/insideout/resources/default-script.clj
+++ PIM/insideout/resources/default-script.clj
@@ -1,4 +1,15 @@
 ;; Default InsideOut startup script
+
+(println "== InsideOut default startup ==")
+(println)
+(println "Running `default-script.clj`.  Override this behavior by creating")
+(println (str (util.core/config :startup-file) " in the current directory."))
+(println)
+(println "If you create `src/`, `test/`, and `resources` folders, these will be")
+(println "automatically included in the classpath in the usual way.")
+(println)
+(println "Starting nrepl...")
+
 (dynamo/resolve-sources)
 (nrepl-server/start! :cider)
 
