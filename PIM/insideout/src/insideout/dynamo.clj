146560d8bccdeae3025adba6f56e957a21fc2f16
David Orme <djo@coconut-palm-software.com>
2021-09-09 22:49:47 -0500

    Initial P2 repo support

--- PIM/insideout/src/insideout/dynamo.clj
+++ PIM/insideout/src/insideout/dynamo.clj
@@ -1,17 +1,46 @@
 (ns insideout.dynamo
   "A component/module system for InsideOut built on classlojure."
   (:require
-   [clojure.core         :refer :all]
-   [clj-foundation.types :as tp]
-   [cemerick.pomegranate :as pom])
-  (:import [java.io File]
-           [java.net URL]
-           [clojure.lang DynamicClassLoader RT]))
+   [cemerick.pomegranate        :as pom]
+   [cemerick.pomegranate.aether :as pom-mvn]
+   [clojure.string              :as str]
+   [clojure.java.io             :as io]
+   [clojure.stacktrace :as stacktrace])
+  (:import
+   [clojure.lang DynamicClassLoader]
+   [java.io File]
+   [org.apache.maven.repository.internal MavenRepositorySystemUtils]
+   [cemerick.pomegranate.aether PomegranateWagonProvider TransferListenerProxy]
+   [org.eclipse.aether RepositorySystem]
+   [org.eclipse.aether.repository LocalRepository]
+   [org.eclipse.aether.transfer TransferListener]
+   [org.eclipse.aether.connector.basic BasicRepositoryConnectorFactory]
+   [org.eclipse.aether.impl DefaultServiceLocator$ErrorHandler]
+   [org.eclipse.aether.spi.connector RepositoryConnectorFactory]
+   [org.eclipse.aether.spi.connector.layout  RepositoryLayoutFactory]
+   [org.eclipse.aether.spi.connector.transport TransporterFactory]
+   [org.eclipse.aether.transport.file FileTransporterFactory]
+   [org.eclipse.aether.transport.wagon WagonTransporterFactory WagonProvider]
+   [org.openntf.maven.p2.connector P2RepositoryConnectorFactory]
+   [org.openntf.maven.p2.layout P2RepositoryLayoutFactory]))
+
+
+(def ^{:private true} default-local-repo
+  (io/file (System/getProperty "user.home") ".m2" "repository"))
+
+(defn eclipse-repo
+  [& YYYY-MM]
+  {:id     "org.eclipse.p2"
+   :url    (str "http://download.eclipse.org/releases/" (or (first YYYY-MM) "2021-09"))
+   :layout "p2"})
 
 
 (def ^:dynamic *extra-repositories*
-  "Extra repositories in addition to Maven Central and Clojars. Default={}"
+  "Extra repositories in addition to Maven Central and Clojars. Default={}.
+  Use the `eclipse-repo` function to get an Eclipse P2 repository configuration
+  if you want to add Eclipse/P2 dependencies."
   {})
+
 
 (defn ^DynamicClassLoader
   dyn-classloader []
@@ -22,26 +51,96 @@
         (-> (Thread/currentThread) (.setContextClassloader dcl))
         dcl))))
 
-#_(defn ^DynamicClassLoader
-    dyn-classloader []
-    (let [cl (var-get Compiler/LOADER)]
-      (if (instance? DynamicClassLoader cl)
-        cl
-        (let [dcl (cond
-                    (instance? ClassLoader cl)              (DynamicClassLoader. cl)
-                    (instance? ClassLoader (RT/baseLoader)) (DynamicClassLoader. (RT/baseLoader))
-                    :default                                (DynamicClassLoader. (.getContextClassLoader (Thread/currentThread))))]
-          (alter-var-root Compiler/LOADER (constantly dcl))
-          dcl))))
-
 (comment
   (dyn-classloader)
   ,)
-#_(def ^DynamicClassLoader
-    dyn-classloader (delay (->> Compiler/LOADER deref .getParent .getParent)))
 
 
-;; Encapsulate cemerick.pomegranate
+;; Stub for supporting https://github.com/OpenNTF/p2-layout-provider
+;;
+;; copypasta from https://github.com/clj-commons/pomegranate/blob/master/src/main/clojure/cemerick/pomegranate/aether.clj#L112
+(defn- repository-system
+  []
+  (let [error-handler (clojure.core/proxy [DefaultServiceLocator$ErrorHandler] []
+                        (serviceCreationFailed [type-clazz impl-clazz ^Throwable e]
+                          (stacktrace/print-cause-trace e)))]
+    (.getService
+     (doto (MavenRepositorySystemUtils/newServiceLocator)
+       (.setService TransporterFactory WagonTransporterFactory)
+       (.setService WagonProvider PomegranateWagonProvider)
+       (.addService RepositoryConnectorFactory BasicRepositoryConnectorFactory)
+       (.addService RepositoryConnectorFactory P2RepositoryConnectorFactory)
+       #_(.addService RepositoryLayoutFactory P2RepositoryLayoutFactory)
+       (.addService TransporterFactory FileTransporterFactory)
+       (.setErrorHandler error-handler))
+     RepositorySystem)))
+
+(defn- default-listener-fn
+  [{:keys [type method transferred resource error] :as evt}]
+  (let [{:keys [name size repository transfer-start-time]} resource]
+    (case type
+      :started (do
+                 (print (case method :get "Retrieving" :put "Sending")
+                        name
+                        (if (neg? size)
+                          ""
+                          (format "(%sk)" (Math/round (double (max 1 (/ size 1024)))))))
+                 (when (< 70 (+ 10 (count name) (count repository)))
+                   (println) (print "    "))
+                 (println (case method :get "from" :put "to") repository))
+      (:corrupted :failed) (when error (println (.getMessage ^Exception error)))
+      nil)))
+
+(defn- transfer-event
+  [^org.eclipse.aether.transfer.TransferEvent e]
+  ;; INITIATED, STARTED, PROGRESSED, CORRUPTED, SUCCEEDED, FAILED
+  {:type (-> e .getType .name str/lower-case keyword)
+   ;; :get :put
+   :method (-> e .getRequestType str/lower-case keyword)
+   :transferred (.getTransferredBytes e)
+   :error (.getException e)
+   :data-buffer (.getDataBuffer e)
+   :data-length (.getDataLength e)
+   :resource (let [r (.getResource e)]
+               {:repository (.getRepositoryUrl r)
+                :name (.getResourceName r)
+                :file (.getFile r)
+                :size (.getContentLength r)
+                :transfer-start-time (.getTransferStartTime r)
+                :trace (.getTrace r)})})
+
+(defn- construct-transfer-listener
+  [transfer-listener]
+  (cond
+    (instance? TransferListener transfer-listener) transfer-listener
+
+    (= transfer-listener :stdout)
+    (TransferListenerProxy. (comp default-listener-fn transfer-event))
+
+    (fn? transfer-listener)
+    (TransferListenerProxy. (comp transfer-listener transfer-event))
+
+    :else (TransferListenerProxy. (fn [_]))))
+
+(defn- repository-session-fn
+  [{:keys [local-repo offline? transfer-listener mirror-selector]}]
+  (let [session (org.apache.maven.repository.internal.MavenRepositorySystemUtils/newSession)
+        session (doto session
+                  (.setLocalRepositoryManager (.newLocalRepositoryManager
+                                               ^RepositorySystem (repository-system)
+                                               session
+                                               (LocalRepository.
+                                                (io/file (or local-repo default-local-repo)))))
+                  (.setMirrorSelector mirror-selector)
+                  (.setOffline (boolean offline?))
+                  (.setTransferListener (construct-transfer-listener transfer-listener)))]
+    (if (contains? (.getConfigProperties session) "aether.checksums.forSignature")
+      session
+      (doto session
+        (.setConfigProperty "aether.checksums.forSignature" true)))))
+
+
+;; Encapsulate cemerick.pomegranate, but adding P2 repo support
 (defn resolve-libs
   "Download and add the specified dependencies to the classpath.  If
   a classloader is specified, use that as the parent classloader else
@@ -50,7 +149,8 @@
   var to add additional repositories beyond these."
 
   ([classloader coordinates]
-   (pom/add-dependencies :classloader classloader
+   (pom/add-dependencies :repository-session-fn repository-session-fn
+                         :classloader classloader
                          :coordinates coordinates
                          :repositories (merge cemerick.pomegranate.aether/maven-central
                                               {"clojars" "https://clojars.org/repo"}
