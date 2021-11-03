a9fde19d925659857dbad4cc127098406e3aed19
David Orme <djo@coconut-palm-software.com>
2021-11-02 22:43:51 -0500

    Nicer example using view-form and a sidebar

--- PIM/insideout/example/src/example/ui/core.clj
+++ PIM/insideout/example/src/example/ui/core.clj
@@ -8,16 +8,37 @@
            [org.eclipse.swt.widgets Display]
            [org.eclipse.swt.layout FillLayout]))
 
+
+(def margin 25)
+
+(defn system-color [name]
+  (-> (Display/getDefault) (.getSystemColor name)))
+
+(defn system-font [name]
+  (-> (Display/getDefault) (.getSystemFont name)))
+
 (defn add-content []
-  (sash-form SWT/VERTICAL (id! :ui/main-sash)
-             (text (| SWT/MULTI SWT/V_SCROLL) (id! :ui/textpane)
-                   (on-modify-text [props _] (println (.getText (:ui/textpane @props)))))
+  (sash-form SWT/HORIZONTAL (id! :ui/main-sash)
+             (view-form (| SWT/BORDER SWT/FLAT)
+                        :margin-height 10
+                        :margin-width 10
+                        :background (system-color SWT/COLOR_INFO_BACKGROUND)
+                        :foreground (system-color SWT/COLOR_INFO_FOREGROUND)
+
+                        (composite (id! :ui/sidebar-title)
+                                   :layout (FillLayout.)
+                                   :background (system-color SWT/COLOR_TITLE_BACKGROUND)
+                                   (label "Contexts && Things"
+                                          :foreground (system-color SWT/COLOR_TITLE_FOREGROUND)))
+                        :top-left :ui/sidebar-title)
+             #_(text (| SWT/MULTI SWT/V_SCROLL) (id! :ui/textpane)
+                     (on-modify-text [props _] (println (.getText (:ui/textpane @props)))))
 
              (browser SWT/WEBKIT (id! :ui/editor)
                       :javascript-enabled true
-                      :url (-> (swtdoc :swt :program 'Program) :result :eclipsedoc))
+                      :url "http://www.google.com" #_(-> (swtdoc :swt :program 'Program) :result :eclipsedoc))
 
-             :weights [20 80]))
+             :weights [25 75]))
 
 
 (defn pre-reload []
