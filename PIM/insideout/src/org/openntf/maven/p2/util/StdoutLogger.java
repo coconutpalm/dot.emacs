0922520f7062b9e73ae4040e6a020bca9da593d0
David Orme <djo@coconut-palm-software.com>
2021-12-15 16:48:46 -0600

    Reformat code

--- PIM/insideout/src/org/openntf/maven/p2/util/StdoutLogger.java
+++ PIM/insideout/src/org/openntf/maven/p2/util/StdoutLogger.java
@@ -4,39 +4,39 @@ import org.eclipse.aether.spi.log.Logger;
 
 public class StdoutLogger {
 
-    public static Logger log = new Logger() {
+	public static Logger log = new Logger() {
 
-        @Override
-        public boolean isDebugEnabled() {
-            return true;
-        }
+		@Override
+		public boolean isDebugEnabled() {
+			return true;
+		}
 
-        @Override
-        public void debug(String msg) {
-            System.out.println("DEBUG: " + msg);
-        }
+		@Override
+		public void debug(String msg) {
+			System.out.println("DEBUG: " + msg);
+		}
 
-        @Override
-        public void debug(String msg, Throwable error) {
-            System.out.println("DEBUG: " + msg);
-            error.printStackTrace();
-        }
+		@Override
+		public void debug(String msg, Throwable error) {
+			System.out.println("DEBUG: " + msg);
+			error.printStackTrace();
+		}
 
-        @Override
-        public boolean isWarnEnabled() {
-            return true;
-        }
+		@Override
+		public boolean isWarnEnabled() {
+			return true;
+		}
 
-        @Override
-        public void warn(String msg) {
-            System.out.println("WARN: " + msg);
-        }
+		@Override
+		public void warn(String msg) {
+			System.out.println("WARN: " + msg);
+		}
 
-        @Override
-        public void warn(String msg, Throwable error) {
-            System.out.println("WARN: " + msg);
-            error.printStackTrace();
-        }
-    };
+		@Override
+		public void warn(String msg, Throwable error) {
+			System.out.println("WARN: " + msg);
+			error.printStackTrace();
+		}
+	};
 
 }
